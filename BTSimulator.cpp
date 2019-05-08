b0491ed81f2e8b7e15d0536a90b6a04230acf89f
rukubrakov <rukubrakov@gmail.com>
2019-05-08 02:07:00 +0200

    BTSimulator.cpp

---
+++ BTSimulator.cpp
@@ -0,0 +1,17 @@
+#include "BTSimulator.hpp"
+
+
+void BTSimulator::run() {
+	while(true){
+		engine_step();
+		racer_step();
+	}
+}
+
+void BTSimulator::engine_step() {
+	
+}
+
+void BTSimulator::racer_step() {
+	
+}
